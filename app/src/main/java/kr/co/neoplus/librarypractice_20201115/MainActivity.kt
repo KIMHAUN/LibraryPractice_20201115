2adbc319505ffe0a2dbd42e98eec0e3dc3e0cb91
kimhaun <wha02068@gmail.com>
2020-11-15 12:40:33 +0900

    Glide를 활용한 웹상의 이미지 불러오기 Manifest에서 인터넷 연결 권한 추가

--- app/src/main/java/kr/co/neoplus/librarypractice_20201115/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/librarypractice_20201115/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.neoplus.librarypractice_20201115
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -21,7 +22,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        Glide.with(mContext).load("https://i.ytimg.com/vi/yPoXG50WrHs/maxresdefault.jpg").into(firstImg)
     }
-
-
 }
